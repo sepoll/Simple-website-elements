bd6757b646afdac9bfdeeb9740a7ebf5a385e1ad
sepoll <s.szubiak@gmail.com>
2015-12-17 19:02:24 +0100

    updated simple tooltip with comments js

--- tooltips/js/app.js
+++ tooltips/js/app.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
 // <span class="tooltipText">of the printing</span>
   var tooltipList = document.querySelectorAll(".tooltip");
   for (var i=0; i<tooltipList.length; i++){
-    tooltipList[i].addEventListener("mouseover", function(event){
+    tooltipList[i].addEventListener("mouseover", function(event){ //czy tu mogłby być this
       var tooltipText = this.dataset.text; //dataset odnosi się do teksu zapisanego w html data-text
-      var newElement = document.createElement("SPAN");
-      newElement.classList.add("tooltipText");
-      newElement.innerHTML = tooltipText;
-      this.appendChild(newElement);
+      var newElement = document.createElement("SPAN"); //zmienna tworząca span
+      newElement.classList.add("tooltipText"); // dodanie klasy do zmiennej
+      newElement.innerHTML = tooltipText; // dodanie tekstu do <spanu>...</span> z dataset zmiennej tooltipText
+      this.appendChild(newElement); // tutaj jest urzyty this
     });
 
     tooltipList[i].addEventListener("mouseout", function(event){
