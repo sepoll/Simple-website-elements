36270aacba8d70e9d49cc0d3afa2605f65fd5378
sepoll <s.szubiak@gmail.com>
2015-12-17 19:00:05 +0100

    updated galery js with comments

--- galery/js/app.js
+++ galery/js/app.js
@@ -4,21 +4,25 @@
   </div>*/
 
   document.addEventListener("DOMContentLoaded", function(){
+
+    //zmienne
      var pictures = document.querySelectorAll(".galery img");
      var body = document.querySelector("body");
 
      for(var i = 0; i<pictures.length; i++){
        pictures[i].addEventListener("click", function(event){
 
+         //zmienne
         var imageSrc = this.getAttribute("src");
         var fullscreendiv = document.createElement("DIV");
         var fullscreenimg = document.createElement("IMG");
         var fullscreenbutton = document.createElement("BUTTON");
 
+        //dodawanie klasy
         fullscreendiv.classList.add("fullscreen");
         fullscreenbutton.classList.add("close");
-        fullscreenbutton.innerHTML = "CLOSE";
-        fullscreenimg.setAttribute("src", imageSrc);
+        fullscreenbutton.innerHTML = "CLOSE"; //tekst dodany do button
+        fullscreenimg.setAttribute("src", imageSrc); //atrybut src otrzymuje src wybranego obrazka
 
         fullscreendiv.appendChild(fullscreenimg);
         fullscreendiv.appendChild(fullscreenbutton);
