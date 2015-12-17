5d90ecb6371971e5c32858a988bd075b27acabc1
sepoll <s.szubiak@gmail.com>
2015-12-17 19:00:47 +0100

    updated menu with comments js

--- menu/js/app.js
+++ menu/js/app.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function(){
   for(var i=0; i<element.length; i++){
     element[i].addEventListener("mouseover", function(event){
       var subList = this.querySelector("ul");
-      if(subList !== null){
-      subList.style.display = "block";
+      if(subList !== null){ // jeśli sublista nie jest pusta
+      subList.style.display = "block"; //otrzymuje display block po najechaniu myszką
     };
     });
 
     element[i].addEventListener("mouseout", function(event){
       var subList = this.querySelector("ul");
       if(subList !== null){
-      subList.style.display = "none";
+      subList.style.display = "none"; // otrzymuje display none po wyjechaniu myszką
     };
     });
   }
